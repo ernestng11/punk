c6e0b77fa66f024fcf018fd225d797aa194a7d74
a180024 <a180024@e.ntu.edu.sg>
2021-09-05 00:27:10 +0800

    Remove backend code

--- lib/provider.ts
+++
@@ -1,6 +0,0 @@
-import { ethers } from "ethers";
-import { env } from "./env";
-
-export function getProvider(): ethers.providers.Provider {
-  return new ethers.providers.JsonRpcProvider(env("TESTNET_URL"));
-}
